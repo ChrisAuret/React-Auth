b87cb18b599b17d888bb683d34e8401943d619b1
Chris Auret <chris.auret@yahoo.co.uk>
2016-05-17 22:08:42 +1000

    Create a user

--- server/models/user.js
+++ server/models/user.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // Define model
-const userSchema = new userSchema({
+const userSchema = new Schema({
     email: { type: String, unique: true },
     password: String
 });
+
+const ModelClass = mongoose.model('user', userSchema);
+
+module.exports = ModelClass;
