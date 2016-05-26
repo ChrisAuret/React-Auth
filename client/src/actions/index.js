2227fcd13b2a877f37c3bf30b4e11d4b2a14212e
Chris Auret <chris.auret@yahoo.co.uk>
2016-05-26 23:45:09 +1000

    Save jwt to localstorage

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,10 +1,25 @@
 // With redux-thunk, not limited to a single action but can dispatch as many as i want
 import axios from 'axios';
+import { browserHistory } from 'react-router';
+import { AUTH_USER } from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
 export function signinUser({ email, password }) {
     return function() {
         axios.post(`${ROOT_URL}/signin`, { email, password })
+            .then(response => {
+                // Update state to indicate user is authenticated
+                dispatch({ type: AUTH_USER }) //redux-thunk
+                
+                //Save JWT token
+                localStorage.setItem('token', response.data.token);
+                
+                browserHistory.push('/feature'); //force navigation
+            })
+            .catch(() =>  {
+                // bad request
+                
+            })
     }
 }
