d0a9d524cfbb86ef65d3ebd4605b69cb3eec2245
Chris Auret <chris.auret@yahoo.co.uk>
2016-05-27 22:26:51 +1000

    start signup

---
+++ client/src/components/auth/signup.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+//import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
+class Signup extends Component {
+    
+    render() {
+        return (
+            <div>Sign up</div>                
+        );
+        
+    }
+}
+
+export default Signup;
