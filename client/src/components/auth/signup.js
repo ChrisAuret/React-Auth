85e082820b7face906010b8edcfed6287d50a595
Chris Auret <chris.auret@yahoo.co.uk>
2016-05-27 22:38:25 +1000

    more signup html

--- client/src/components/auth/signup.js
+++ client/src/components/auth/signup.js
@@ -5,12 +5,50 @@ import * as actions from '../../actions';
 
 class Signup extends Component {
     
-    render() {
+    handleFormSubmit(formProps) {
+        // Call action creator to sign up the user!
+        this.props.signupUser(formProps);
+    }
+
+    renderAlert() {
+        if (this.props.errorMessage) {
+            return (
+            <div className="alert alert-danger">
+                <strong>Oops!</strong> {this.props.errorMessage}
+            </div>
+            );
+        }
+    }
+    
+    render() {     
+        const { handleSubmit, fields: { email, password, passwordConfirm }} = this.props;
+        
         return (
-            <div>Sign up</div>                
+            <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+                <fieldset className="form-group">
+                    <label>Email:</label>
+                    <input className="form-control" {...email} />
+                    {email.touched && email.error && <div className="error">{email.error}</div>}
+                </fieldset>
+                <fieldset className="form-group">
+                    <label>Password:</label>
+                    <input className="form-control" {...password} type="password" />
+                    {password.touched && password.error && <div className="error">{password.error}</div>}
+                </fieldset>
+                <fieldset className="form-group">
+                    <label>Confirm Password:</label>
+                    <input className="form-control" {...passwordConfirm} type="password" />
+                    {passwordConfirm.touched && passwordConfirm.error && <div className="error">{passwordConfirm.error}</div>}
+                </fieldset>
+                {this.renderAlert()}
+                <button action="submit" className="btn btn-primary">Sign up!</button>
+            </form>              
         );
         
     }
 }
 
-export default Signup;
+export default reduxForm({
+  form: 'Signup',
+  fields: ['email', 'password', 'passwordConfirm']  
+})(Signup);
