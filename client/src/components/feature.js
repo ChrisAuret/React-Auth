364f2ad2e9b14c15c9afc595f2fd8c81809fad11
Chris Auret <chris.auret@yahoo.co.uk>
2016-05-28 12:31:32 +1000

    handle authenticated requests.FIN

--- client/src/components/feature.js
+++ client/src/components/feature.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router';
+import * as actions from '../actions'; 
 
 class Feature extends Component {
-    render(){
+    
+    componentWillMount() {
+        this.props.fetchMessage();
+    }
+    
+    render() {
         return (
-            <div>this is a feature </div>
-        )
+            <div>{this.props.message}</div>
+        );
     }
 }
 
-export default Feature;
+function mapStateToProps(state) {
+    return { message: state.auth.message };
+}
+
+export default connect(mapStateToProps, actions)(Feature);
